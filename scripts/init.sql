4e5b88737b2b4417f8ae2feb128dabb5ca7b711a
LzrCampos <lazarocampos98@gmail.com>
2018-06-16 21:10:07 -0300

    Alterando nome da coluna

--- scripts/init.sql
+++ scripts/init.sql
@@ -6,5 +6,5 @@ CREATE TABLE emails (
     id serial not null,
     criacao timestamp not null default current_timestamp,
     assunto varchar(100) not null,
-    mensagem varchar(2500) not NULL
+    mensagem varchar(2500) not null
 );
